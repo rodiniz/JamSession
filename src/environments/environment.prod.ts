8d1dbe7a0231f9902f386fa140b013d0c42213c9
rochagasdiniz@gmail.com <rochagasdiniz@gmail.com>
2025-04-12 16:42:14 +0100

    add variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   apiUrl: 'https://api.your-production-domain.com',
   appName: 'Music Jam',
   version: '0.1.0',
-  // Add any other configuration variables you need
+  apiKey: '8aee4829-8a3b-434c-8975-0c539761bc19',
+  apiSecret: 'bb0bf375e2083fa3c95e79d805537caab45f3edfcaccd23870141cde9f7ea79b',
 };
